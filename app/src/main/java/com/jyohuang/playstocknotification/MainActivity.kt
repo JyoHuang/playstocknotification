e8b91ec2c7dc9e0a599e6381459431ff84f2308c
JYO <createdigit@gmail.com>
2025-11-22 14:08:20 +0800

    完成登入頁面的基本畫面

--- app/src/main/java/com/jyohuang/playstocknotification/MainActivity.kt
+++ app/src/main/java/com/jyohuang/playstocknotification/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jyohuang.playstocknotification.ui.theme.PlaystocknotificationTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,12 +58,66 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 /**
  * 登入頁面
  */
+@Composable
+fun LoginScreen(
+
+){
+    var account by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Surface (
+        modifier = Modifier.fillMaxSize()
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = "股票價格通知系統",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+
+            OutlinedTextField(
+                value = account,
+                onValueChange = { account = it},
+                label = {Text("帳號")},
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = {password = it},
+                label = {Text("密碼")},
+                visualTransformation = PasswordVisualTransformation(),
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+
+                }
+            ) {
+                Text("登入")
+            }
+        }
+    }
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PlaystocknotificationTheme {
-        Greeting("Joey")
+        LoginScreen(
+
+        )
     }
 }
